dad299321dc2a344aad49f80ed50aff8b163928d
apocryph575 <apocryph575@gmail.com>
2024-03-02 14:53:40 +0100

    Last

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ def image_metadata(image_path):
     # check image metadata
     with open(image_path, "rb") as palm_1_file:
         exif_data = exif.Image(palm_1_file)
-        all_attributes = exif_data.get_all()
     if exif_data.has_exif:
         status = f"contains EXIF (version {exif_data.exif_version}) information."
     else:
         status = "does not contain any EXIF information."
     print(f"Image {status}")
+
+    # Get all attributes
+    all_attributes = exif_data.get_all()
 
     # Show all attributes
     for tag, value in all_attributes.items():
@@ -18,7 +20,7 @@ def image_metadata(image_path):
 
 
 def main():
-    image_path = 'images/test.jpg'
+    image_path = 'images/example (1).jpg'
     image_metadata(image_path)
 
 
